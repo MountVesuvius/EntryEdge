c882e8b6124b296b202a83da53f6577d40b9aa14
MountVesuvius <JudeALakkis@gmail.com>
2023-08-12 01:15:08 +1000

    Basic styling and page setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import "../app.css";
 </script>
+<nav>
+  <div class="flex items-center m-6">
+    <img src="logo.svg" alt="Logo" class="w-10 sm:w-14">
+    <span style="font-family: 'Nunito';" class="text-white text-lg sm:text-2xl font-black">EntryEdge</span>
+  </div>
+</nav>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Posting from "$lib/components/Posting.svelte";
+    import jobs from '$lib/data/jobs.json';
+
+    const colors = [
+        "#FFE1CC", 
+        "#FFD1E8", 
+        "#E1FFD1", 
+        "#CCF5FF", 
+        "#FFCCF2", 
+        "#F2DFFF", 
+        "#E0E4FF", 
+        "#D5FFFB", 
+        "#FFFAE1", 
+    ]
+
+    function pickRandomColor() {
+        let picked = colors[Math.floor(Math.random() * colors.length)];
+        colors.splice(colors.indexOf(picked), 1);
+        return picked;
+    }
+
+</script>
+
+<div class="w-screen flex flex-col items-center justify-center text-center text-white mb-6">
+    <h1 class="text-[1.5rem] sm:text-[4rem] font-bold">Find Your First Student Job</h1>
+    <h3 class="sm:text-[2rem]">Launching your careers, the right way</h3>
+</div>
+
+<!-- filters -->
+<!-- <div class="">
+
+</div> -->
+
+
+<div class="flex flex-col flex-wrap sm:flex-row mx-4 justify-center">
+    {#each jobs.jobPostings as job}
+        <Posting
+            color={pickRandomColor()}
+            date={job.postedDate}
+            company={job.company}
+            title={job.title}
+            location={job.location}
+            salary={job.hourlyRate}
+            tags={job.tags}
+        />
+    {/each}
+</div>
