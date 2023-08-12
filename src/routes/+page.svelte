0425d6d86cd94ba08526e4445c340a7c8d50d056
MountVesuvius <JudeALakkis@gmail.com>
2023-08-12 22:53:13 +1000

    fixed hover effect bug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
 
 
 <div class="flex flex-col justify-center sm:ml-[8%]">
-    <button on:click={() => {isJobTypeOpen = !isJobTypeOpen;}} class="text-white bg-blue-600 w-60 flex items-center justify-center py-2 rounded-md">
+    <button on:click={() => {isJobTypeOpen = !isJobTypeOpen;}} class="text-white bg-blue-600 w-60 flex items-center justify-center py-2 rounded-md hover:bg-blue-700">
         Job Type
         <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
