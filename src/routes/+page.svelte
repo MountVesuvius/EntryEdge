1b11aa0a948eaa837ecb2b6578a048a1a6991c11
MountVesuvius <JudeALakkis@gmail.com>
2023-08-13 22:06:38 +1000

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@
 
 <div class="w-screen flex flex-col items-center justify-center text-center text-white mb-6">
     <h1 class="text-[1.5rem] sm:text-[4rem] font-bold typed">Find Your First Student Job</h1>
-    <h3 class="sm:text-[2rem]">Launching your careers, the right way</h3>
+    <h3 class="sm:text-[2rem]">Launching your careers, the right way. just for dean</h3>
 </div>
 
 
